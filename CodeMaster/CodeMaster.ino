729b8203565a77c6be41ec2ed4c936dd92da9ca6
Kevin Campo <kevinerneycertuche@gmail.com>
2024-03-23 20:02:53 -0500

    Principal cambio crear un nuevo puerto serie exclusivo para enviar la informacio, pruebas con comandos para encender y apagar un led, ademas de leer el valor de un potenciometro

--- CodeMaster/CodeMaster.ino
+++ CodeMaster/CodeMaster.ino
@@ -1,57 +1,78 @@
-const int ledPin =  13; // Numero del pin para el Led     
-const int EnTxPin =  2; // HIGH:TX y LOW:RX
-void setup() 
-{ 
-  Serial.begin(9600);
-  Serial.setTimeout(100); //establecemos un tiempo de espera de 100ms
-  // inicializamos los pines
-  pinMode(ledPin, OUTPUT);
-  pinMode(EnTxPin, OUTPUT);
-  digitalWrite(ledPin, LOW); 
-  digitalWrite(EnTxPin, HIGH); //RS485 como Transmisor
-} 
- 
-void loop() 
-{ 
-   
-  int lectura = analogRead(0);//leemos el valor del potenciómetro (de 0 a 1023) 
-  int angulo= map(lectura, 0, 1023, 0, 180); // escalamos la lectura a un valor de ángulo (entre 0 y 180)
-  //---enviamos el ángulo para mover el servo------
-  Serial.print("I"); //inicio de trama
-  Serial.print("102");//dirección del esclavo
-  Serial.print("S"); //función  S para indicarle que vamos a mover el servo
-  Serial.print(angulo); //ángulo  o dato
-  Serial.print("F"); //fin de trama
-  //----------------------------
-  delay(50); 
-  //---solicitamos una lectura del sensor----------
-  Serial.print("I"); //inicio de trama
-  Serial.print("102");//direccion del esclavo
-  Serial.print("L"); //L para indicarle que vamos a Leer el sensor
-  Serial.print("F"); //fin de trama
-  Serial.flush();    //Esperamos hasta que se envíen los datos
-  //----Leemos la respuesta del Esclavo-----
-  digitalWrite(EnTxPin, LOW); //RS485 como receptor
-  if(Serial.find("i")) //esperamos el inicio de trama
-  {
-      int esclavo=Serial.parseInt();  //recibimos la direccion del esclavo
-      int dato=Serial.parseInt();  //recibimos el dato
-      if(Serial.read()=='f'&&esclavo==102) //si fin de trama y direccion son los correctos
-      {
-         funcion(dato);   //realizamos la acción con el dato recibido
-      }
-  }
-      digitalWrite(EnTxPin, HIGH); //RS485 como Transmisor
-  //----------fin de la respuesta----------
-  
-} 
-void funcion(int dato)
-{
-  //hjhdjwhjwdhdw
-  if(dato>500)
-  digitalWrite(ledPin, HIGH); 
-  else{
-    digitalWrite(ledPin, LOW); 
-  }
+#include <SoftwareSerial.h>  //Crea nuevos puertos serie
 
+//Prueba
+#define HEAD         0xAA  //Encabezado de la trama
+#define TAIL         0xFE  //Bit de STOP
+#define CMD_LED_ON   0x01  //Enceder Led
+#define CMD_LED_OFF  0x02  //Apagar led
+#define CMD_READ_POT 0x03  //Solicitar el valor del sensor
+#define SLAVE        0x23  //Direccion especifica del esclavo
+
+#define RS485_BAUD 9600   // Velocidad de comunicación RS-485
+#define RS485_PIN_MODE 2  // HIGH: Tx; LOW: Rx
+
+SoftwareSerial RS485Serial(3, 4);  // RX, TX
+byte trama[5], idx;                //Cadena para concatenar la informacion
+
+void setup() {
+  Serial.begin(19200);  // Inicializar comunicación serial para monitor
+  pinMode(RS485_PIN_MODE, OUTPUT);
+  RS485Serial.begin(RS485_BAUD);
+}
+
+void loop() {
+
+  if (Serial.available()) {
+    char c = Serial.read();
+    int h, t;
+    switch (c) {
+      case '0':
+        enviarComando(SLAVE, CMD_LED_ON);
+        break;
+
+      case '1':
+        enviarComando(SLAVE, CMD_LED_OFF);
+        break;
+
+      case '2':
+        enviarComando(SLAVE, CMD_READ_POT);
+        Serial.print("Potenciometro: ");
+        t = recibirRespuesta(SLAVE);
+        if (t == -1)
+          Serial.println("No se recibio' respuesta");
+        else
+          Serial.println(t);
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
+//Funcion para enviar un paquete de datos
+void enviarComando(byte esclavo, byte cmd) {
+  trama[0] = HEAD;
+  trama[1] = esclavo;
+  trama[2] = cmd;
+  trama[3] = TAIL;
+  digitalWrite(RS485_PIN_MODE, HIGH);  // modo tx
+  RS485Serial.write(trama, 4);
+  RS485Serial.flush();
+  digitalWrite(RS485_PIN_MODE, LOW);  // modo rx
+  Serial.print(trama[0]);
+}
+
+//Funcion para recibir la respuesta del esclavo
+int recibirRespuesta(byte esclavo) {
+  digitalWrite(RS485_PIN_MODE, LOW);  // modo rx
+  delay(500);
+  RS485Serial.readBytes(trama, 4);
+  if (trama[0] != HEAD)  // error en la trama
+    return -1;
+  if (trama[1] != esclavo)  // respuesta de otro esclavo
+    return -1;
+  if (trama[3] != TAIL)  // error en trama
+    return -1;
+  return trama[2];
 }
--- CodeSlave1/CodeSlave1.ino
+++ CodeSlave1/CodeSlave1.ino
@@ -1,58 +1,103 @@
-#include <Servo.h> 
- 
-Servo myservo;  // creamos el objeto servo 
-const int EnTxPin =  2;  // HIGH:TX y LOW:RX
-const int mydireccion =101; //Direccion del esclavo
-void setup() 
-{ 
-  Serial.begin(9600);  
-  Serial.setTimeout(100);  //establecemos un tiempo de espera de 100ms
-  myservo.attach(9);  // asignamos el pin 9 para el servo.
-  pinMode(EnTxPin, OUTPUT);
-  digitalWrite(EnTxPin, LOW); //RS485 como receptor
-} 
- 
-void loop() 
-{ 
-  if(Serial.available())
-  {
-    if(Serial.read()=='I') //Si recibimos el inicio de trama
-    {
-        int direccion=Serial.parseInt(); //recibimos la direccion 
-        if(direccion==mydireccion) //Si direccion es la nuestra
-        {
-            char funcion=Serial.read(); //leemos el carácter de función
-          
-            //---Si el carácter de función es una S entonces la trama es para mover el motor----------- 
-            if(funcion=='S') 
-             {
-                 int angulo=Serial.parseInt(); //recibimos el ángulo
-                 if(Serial.read()=='F') //Si el fin de trama es el correcto
-                 {
-                   if(angulo<=180) //verificamos que sea un valor en el rango del servo
-                    {
-                      myservo.write(angulo); //movemos el servomotor al ángulo correspondiente.
-                    }   
-                 }
-             }
-             //---Si el carácter de función  es L entonces el maestro está solicitando una lectura del sensor---
-             else if(funcion=='L')
-             {
-                if(Serial.read()=='F') //Si el fin de trama es el correcto
-                 {
-                   int lectura = analogRead(0); //realizamos  la lectura del sensor     
-                   digitalWrite(EnTxPin, HIGH); //rs485 como transmisor
-                    Serial.print("i"); //inicio de trama  
-                   Serial.print(mydireccion); //direccion   
-                   Serial.print(",");       
-                   Serial.print(lectura); //valor del sensor
-                   Serial.print("f"); //fin de trama  
-                   Serial.flush(); //Esperamos hasta que se envíen los datos
-                   digitalWrite(EnTxPin, LOW); //RS485 como receptor             
-                 }
-             }
-        }
+#include <SoftwareSerial.h>
+
+// Trama maestro-esclavo
+#define HEAD 0xAA
+#define MY_SLAVE_ID 0x23
+#define TAIL 0xFE
+
+//Comandos soportados
+#define CMD_LED_ON      0x01
+#define CMD_LED_OFF     0x02
+#define CMD_READ_POT    0x03
+
+#define LED_BUILTIN     13
+
+// Formato de Trama: <HEAD> <SLAVE_ID> <CMD> <TAIL>
+
+#define RS485_PIN_MODE 9         // HIGH -> Transmision; LOW-> recepcion
+
+SoftwareSerial RS485Serial(10, 8);    // RX, TX
+
+byte buff[5], idx;
+float potenciometro;
+
+void enviarRespuesta( float x ){
+  buff[0] = HEAD;
+  buff[1] = MY_SLAVE_ID;
+  buff[2] = (byte)x;
+  buff[3] = TAIL;
+  digitalWrite( RS485_PIN_MODE, HIGH ); // poner en modo Tx
+  RS485Serial.write( buff, 4 );               // transmitir mensaje
+  RS485Serial.flush();
+  digitalWrite( RS485_PIN_MODE, LOW);   // poner en modo Rx
+  
+}
+
+void ejecutarComando(){
+  Serial.println("Ejectutando comando!!!");
+  if ( buff[1] != MY_SLAVE_ID ) // el mensaje es para otro esclavo
+    return;
+
+  switch( buff[2] ){                      // ejecutar comando
+
+    case CMD_LED_ON:                      // Encender Led
+      digitalWrite( LED_BUILTIN, HIGH );  
+      break;
+
+    case CMD_LED_OFF:                     // Apagar Led
+      digitalWrite( LED_BUILTIN, LOW );
+      break;
+    
+    case CMD_READ_POT:
+      potenciometro = analogRead(0);
+      enviarRespuesta( potenciometro );
+      break;
+
+    default:                              // Comando Inva'lido
+      break;
+  }
+}
+
+
+void setup() {
+  // Configurar Serial a 19200 baudios (para el monitor serie)
+  Serial.begin(19200);
+
+  // Configurar para utilizar el bus 485 a 9600
+  RS485Serial.begin(9600);
+
+  // configrar pines
+  pinMode( LED_BUILTIN, OUTPUT );
+  pinMode( RS485_PIN_MODE, OUTPUT );
+  digitalWrite( LED_BUILTIN, LOW );   // apagar led
+  digitalWrite( RS485_PIN_MODE, LOW );// poner en modo de recepcion
+  idx = 0;
+}
+
+
+void loop() {
+
+  if( !RS485Serial.available() )
+    return;
+
+  byte incoming = RS485Serial.read();
+  Serial.print("Recibido: ");
+  Serial.println(incoming);
+    
+  if( idx == 0 ){           // principio de trama
+    if( incoming != HEAD ) // trama incorrecta
+      return;
+
+    buff[idx] = incoming;
+    idx++;
+  }
+  else if ( idx > 0 && idx < 4 ){ // 
+    buff[idx++] = incoming;      //
+     
+    if ( idx == 4 ){                // fin de trama
+      if( buff[3] == TAIL )         // verificar que termine bien
+        ejecutarComando();
+      idx = 0;
     }
   }
-  delay(10);
-} 
+}
